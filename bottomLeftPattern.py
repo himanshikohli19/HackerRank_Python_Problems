6a36693b37b586ea4613bc5dffa565fc9fb1b72c
himanshikohli19 <himanshi.kohli19@gmail.com>
2021-06-07 14:21:35 +0530

    Bottom Left Pattern Problem

---
+++ bottomLeftPattern.py
@@ -0,0 +1,9 @@
+#top left pattern
+n=7
+for i in range(1,8): 
+    for j in range(1,n):
+        if i<=j:
+            print(" ",end=" ")
+        else:
+            print("*",end=" ")    
+    print()
