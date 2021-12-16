d204379e029344d404028927fa65e2d6a13f5a00
himanshikohli19 <himanshi.kohli19@gmail.com>
2021-12-16 15:46:47 +0530

    set add

---
+++ set-add.py
@@ -0,0 +1,29 @@
+'''
+sets.add() problem - HackerRank
+
+Task
+Apply your knowledge of the .add() operation to help your friend Rupal.
+
+Rupal has a huge collection of country stamps.
+She decided to count the total number of distinct country stamps in
+her collection. She asked for your help. You pick the stamps one by
+one from a stack of  country stamps.
+
+Find the total number of distinct country stamps.
+
+Input Format
+The first line contains an integer , the total number of country stamps.
+The next  lines contains the name of the country where the stamp is from.
+
+Output Format
+Output the total number of distinct country stamps on a single line.
+'''
+
+N = int(input())
+lis =[]
+c=0
+for i in range(N):
+    str1=input()
+    lis.append(str1)
+set1 = set(lis)
+print(len(set1))
