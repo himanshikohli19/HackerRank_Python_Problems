d5e86d0d9fe0396e222ee3cede700c056b5a81e8
himanshikohli19 <himanshi.kohli19@gmail.com>
2022-02-07 15:10:44 +0530

    FizzBuzz

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -3,24 +3,24 @@ FizzBuzz Problem:
 
 Given a number n, for each integer i in the range from 1 to n inclusive, print
 one value per line as follows:
-- if i is a multiple of both 3 and 5, print FizzBuzz
-- If i is multiple of 3 (but not 5) print Fizz.
-- If i is multiple of 5 (but not 3) print Fizz.
-- If i is not a multiple of 3 or 5, print value of i
+- if i is a multiple of both 4 and 5, print FizzBuzz
+- If i is multiple of 4 (but not 5) print Fizz.
+- If i is multiple of 5 (but not 4) print Fizz.
+- If i is not a multiple of 4 or 5, print value of i
 '''
+#TIME COMPLEXITY: O(N)
 
-def fizzBuzz(n):
-    for i in range(1,n+1):
-        if i%3==0 and i%5==0:
+def FizzBuzzProblem(N):
+    for i in range(1,N+1):
+        if i%4==0 and i%5==0:
             print("FizzBuzz")
-        elif i%3==0:
+        elif i%4==0:
             print("Fizz")
         elif i%5==0:
             print("Buzz")
         else:
             print(i)
-    # Write your code here
+   
 if __name__ == '__main__':
-    n = int(input().strip())
-
-    fizzBuzz(n)
+    n = 30
+    FizzBuzzProblem(n)
