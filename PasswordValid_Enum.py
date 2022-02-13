11c3dc5bc5660675025f876be07bccb3105e0985
himanshikohli19 <himanshi.kohli19@gmail.com>
2022-02-13 22:44:18 +0530

    Password validity usiNG Enum

--- PasswordValid_Enum.py
+++ PasswordValid_Enum.py
@@ -1,4 +1,5 @@
 '''
+CHECK PASSWORD VALIDITY:
 Write a Python program to check the validity of password input by users.
 - At least 1 letter between [a-z] and 1 letter between [A-Z].
 - At least 1 number between [0-9].
